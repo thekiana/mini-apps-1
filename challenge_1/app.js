5702b0bc2502b3c42ec9ed82e31198d08708da00
KKD <kianakdonaldson@gmail.com>
2020-04-18 08:26:15 -0700

    Create click handler and alternate player functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,18 @@
+let currentPlayer = 'O';
 
+function handlePlayerChange() {
+  currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
+  return currentPlayer;
+}
 
+function handlePlay(e) {
+  const rows = document.querySelectorAll('#table td');
+
+  rows.forEach(row => {
+    if (e.target === row) {
+      row.innerHTML = handlePlayerChange();
+    }
+  });
+}
+
+window.addEventListener('click', handlePlay);
