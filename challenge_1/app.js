c189168a5ecd38785dc6f78c36246e2260889992
KKD <kianakdonaldson@gmail.com>
2020-04-19 08:36:25 -0700

    Implement state handler functionality

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -11,37 +11,38 @@ function handlePlay(e) {
   boxes.forEach(box => {
     if (e.target === box && box.innerHTML === '') {
       box.innerHTML = handlePlayerChange();
-      handleState(boxes);
     }
   });
 
-  // handleWin(boxes);
+  const state = handleState(boxes);
+  // console.log('state in play handler ', state);
+  handleWin(state);
 }
 
-// function handleWin(boxes) {
-//   const winningState = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8]
-//   ];
- 
-//   const player = 'X';
-//   const state = handleState(boxes);
-//   console.log(state);
+function handleWin(state) {
+  const winningState = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ];
 
+  const numPlays = Object.keys(state).length;
 
-//   // for (var i = 0; i < state.length; i++) {
-//   //   if (state === winningState[i]) {
-//   //     console.log(state, ' -- state');
-//   //     console.log(winningState[i], ' -- winningState[i]');
-//   //     alert(`Player ${player} won!`);
-//   //   }
-//   // }
-// }
+  if (numPlays === 9) {
+    setTimeout(() => {
+      alert(`Game over! Reset game to play.`);
+    }, 300);
+    // clearTimeout();
+  }
+
+  // state is an object
+  // iterate through and compare
+
+  // alert(`Player ${player} won!`);
+}
 
 function handleState(boxes) {
   var currentState = {};
-  var objLength = Object.keys(currentState).length;
 
   
   for (var i = 0; i < boxes.length; i++) {
@@ -55,7 +56,7 @@ function handleState(boxes) {
       currentState[getIndex] = box.innerHTML;
     }
   }
-  console.log(currentState);
+
   return currentState;
 }
 
@@ -63,7 +64,7 @@ function handleGameReset() {
   location.reload();
 };
 
-window.addEventListener('click', handlePlay);
+document.querySelector('#table').addEventListener('click', handlePlay);
 document.querySelector('#button').addEventListener('click', handleGameReset);
 
 // After each play, look for 3 in a row,
